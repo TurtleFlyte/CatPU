543258bb58cfe34997cd7be14717c6d85b875321
TurtleFlyte <49802391+TurtleFlyte@users.noreply.github.com>
2025-07-25 11:31:53 -0700

    performance fetching and basic animated icon

---
+++ CatPU/AnimatedIconView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedIconView.swift
+//  CatPU
+//
+//  Created by Andrew Polkinhorn on 7/25/25.
+//
+
+import SwiftUI
+
+struct AnimatedIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedIconView()
+}
